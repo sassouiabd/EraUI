8ca12f782fa32b85d474535279094f12d02c9b7f
SASSOUI Abdel <sassouiabd@gmail.com>
2019-12-23 15:27:31 +0100

    AutoRepair and AutoSell grey items

--- EraUI.lua
+++ EraUI.lua
@@ -240,6 +240,52 @@ local function ModifyMiniMap()
 
 end
 
+--********************************
+--            AutosSell
+--********************************
+local function AutoSellAndRepair()
+
+	-- Autosell grey trash and repair
+	--Don't Change anything here
+	local g = CreateFrame("Frame")
+	g:RegisterEvent("MERCHANT_SHOW")
+
+	g:SetScript("OnEvent", function()  
+			local bag, slot
+			for bag = 0, 4 do
+					for slot = 0, GetContainerNumSlots(bag) do
+							local link = GetContainerItemLink(bag, slot)
+							if link and (select(3, GetItemInfo(link)) == 0) then
+									UseContainerItem(bag, slot)
+							end
+					end
+			end
+
+			if(CanMerchantRepair()) then
+					local cost = GetRepairAllCost()
+					if cost > 0 then
+							local money = GetMoney()
+							if IsInGuild() then
+									local guildMoney = GetGuildBankWithdrawMoney()
+									if guildMoney > GetGuildBankMoney() then
+											guildMoney = GetGuildBankMoney()
+									end
+									if guildMoney > cost and CanGuildBankRepair() then
+											RepairAllItems(1)
+											print(format("|cfff07100Repair cost covered by G-Bank: %.1fg|r", cost * 0.0001))
+											return
+									end
+							end
+							if money > cost then
+									RepairAllItems()
+									print(format("|cffead000Repair cost: %.1fg|r", cost * 0.0001))
+							else
+									print("Not enough gold to cover the repair cost.")
+							end
+					end
+			end
+	end)
+end
 
 --********************************
 --            QuestTracker
@@ -289,6 +335,8 @@ function EraUI_Update()
 	EliteFrame();
 	
 	ModifyMiniMap();
+	
+	AutoSellAndRepair();
 
 end
 
